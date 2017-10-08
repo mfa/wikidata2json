ae18cfc2bbebe8267c630095d063b3cdd973d4a8
Andreas Madsack <andreas@madflex.de>
2017-10-08 16:36:24 +0200

    add json input and write to bz2 compressed output

--- main.py
+++ main.py
@@ -1,33 +1,54 @@
 import asyncio
+import bz2
 import glob
 import json
+import os
 
 import aiofiles as aiof
 import dateutil.parser
 from lxml import etree, objectify
 
 
-async def export_page(title, data, dt):
-    async with aiof.open(f'output/{title}.json', 'w') as fp:
-        await fp.write(json.dumps(json.loads(data.replace('&quot;', '"'))))
+async def export_page(title, data):
+    filename = f'output/{title}.json.bz2'
+    async with aiof.open(filename, 'wb') as fp:
+        compressed_data = bz2.compress(json.dumps(data, indent=4, sort_keys=True).encode())
+        await fp.write(compressed_data)
         await fp.flush()
 
 
-async def process_file(filename):
+async def process_file_xml(filename, rename):
     for event, element in etree.iterparse(open(filename, 'rb'), tag='{*}page'):
         title = element.find('title', namespaces=element.nsmap).text
         if title.startswith(('Q', 'P')):
             revision = element.find('revision', namespaces=element.nsmap)
             data = revision.find('text', namespaces=element.nsmap).text
-            isotime = revision.find('timestamp', namespaces=element.nsmap).text
-            dt = dateutil.parser.parse(isotime)
-            await export_page(title, data, dt)
+            # isotime = revision.find('timestamp', namespaces=element.nsmap).text
+            # dt = dateutil.parser.parse(isotime)
+            await export_page(title, json.loads(data.replace('&quot;', '"')))
+    if rename:
+        os.rename(filename, filename + '.done')
 
 
-async def process():
-    for fn in glob.glob('input/*'):
-        await process_file(fn)
+async def process_file_json(filename, rename):
+    async with aiof.open(filename) as fp:
+        async for line in fp:
+            if line.strip() in ['[', ']']:
+                continue
+            line = line.strip()
+            line = line[:-1] if line[-1] == ',' else line
+            data = json.loads(line)
+            await export_page(data['id'], data)
+    if rename:
+        os.rename(filename, filename + '.done')
+
+
+async def process(rename):
+    for fn in glob.glob('input/*.xml'):
+        await process_file_xml(fn, rename)
+    for fn in glob.glob('input/*.json'):
+        await process_file_json(fn, rename)
 
 
 loop = asyncio.get_event_loop()
-server = loop.run_until_complete(process())
+loop.run_until_complete(process(rename=False))
